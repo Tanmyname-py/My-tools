07582da58a36a8cd33aa84512bdf10394943d828
Tanmyname <sultanbahri2304l@gmail.com>
2026-01-03 22:26:53 +0800

    Add type check for string in toCapiTalize function

--- src/capitalize.js
+++ src/capitalize.js
@@ -3,6 +3,7 @@
 */
 
 const toCapiTalize = (string) => {
+  if(typeof string !== "string") throw new TypeError('Args must be string');
   const text = string.split(" ").map((t) => {
     let cptz = t.charAt(0).toUpperCase();
     cptz =  cptz + t.slice(1).toLowerCase();
